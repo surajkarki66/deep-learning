3910b651324d0fec8af56f328ac098576974ef8a
surajkarki66 <suraj.karki500@gmail.com>
2020-05-11 11:55:16 +0545

    Finished word_representation

---
+++ Deep Learning/Tensorflow/RNN/word_representation/utils.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+def read_glove_vecs(glove_file):
+    with open(glove_file, 'r') as f:
+        words = set()
+        word_to_vec_map = {}
+        
+        for line in f:
+            line = line.strip().split()
+            curr_word = line[0]
+            words.add(curr_word)
+            word_to_vec_map[curr_word] = np.array(line[1:], dtype=np.float64)
+            
+    return words, word_to_vec_map
+    
