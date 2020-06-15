c0e14247e0e1dc4f49bd72a52fe586ed35bbd48b
Suraj Karki <suraj.karki500@gmail.com>
2020-06-15 19:29:59 +0545

    Finished the application of traffic_sign_detection

--- Deep_Learning_Projects/traffic_sign_detection/gui.py
+++ Deep_Learning_Projects/traffic_sign_detection/gui.py
@@ -1,79 +1,95 @@
 import tkinter as tk
 import pandas as pd
 import pickle
+import numpy as np
 
 from tkinter import filedialog
 from tkinter import *
 from PIL import ImageTk, Image
-from tensorflow.keras.models import load_model
-
-pickle_in=open("traffic_model.p","rb")  ## rb = READ BYTE
-model=pickle.load(pickle_in)
-
-def get_classname(class_no):
-    data = pd.read_csv('labels.csv')
-    classname= None 
-    for class_id, class_name in data.values:
-        if class_no == class_id:
-            classname = class_name
-
-    return classname
-
-def classify(file_path):
-    image = Image.open(file_path)
-    image = image.resize((32, 32))
-    image = np.expand_dims(image, axis=0)
-    image = np.array(image)
-    pred = model.predict([image])[0]
-    pred = np.argmax(pred)
-    sign = get_classname(pred)
-    label.configure(foreground='#011638', text=sign)
+from keras.models import load_model
 
 
-def show_classify_button(file_path):
-    classify_b = Button(top, text="Classify Image",
-                        command=lambda: classify(file_path), padx=10, pady=5)
-    classify_b.configure(background='#364156', foreground='white',
-                         font=('arial', 10, 'bold'))
-    classify_b.place(relx=0.79, rely=0.46)
+class MainWindow(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.title("TRAFFIC SIGN CLASSIFIER")
+        self.geometry("800x600")
+        self.resizable(False, False)
+        self.configure(background='#CDCDCD')
 
+        # Defininf labels
+        self.class_name = Label(
+            self, background='#CDCDCD', font=('arial', 20, 'bold'))
+        self.accuracy_label = Label(self, font=('arial', 20, 'bold'))
+        self.class_image = Label(self)
+        self.definition = Label(
+            self, background='#CDCDCD', font=('arial', 20, 'bold'))
 
-def upload_image():
-    try:
-        file_path = filedialog.askopenfilename()
-        uploaded = Image.open(file_path)
-        uploaded.thumbnail(
-            ((top.winfo_width()/2.25), (top.winfo_height()/2.25)))
-        im = ImageTk.PhotoImage(uploaded)
-        sign_image.configure(image=im)
-        sign_image.image = im
-        label.configure(text='')
-        show_classify_button(file_path)
-    except:
-        pass
+        self.upload = Button(self, text="Upload an image", command=self.upload_image,
+                             padx=10, pady=5)
 
+        self.upload.configure(background='#364156', foreground='white',
+                              font=('arial', 10, 'bold'))
+        self.upload.pack(side=BOTTOM, pady=50)
+        self.definition.pack(side=BOTTOM)
+        self.class_image.pack(side=BOTTOM, expand=True)
+        self.accuracy_label.pack(side=BOTTOM, expand=True)
+        self.class_name.pack(side=BOTTOM, expand=True)
+        self.heading = Label(self, text="TRAFFIC SIGN CLASSIFIER",
+                             pady=20, font=('arial', 30, 'bold'))
+        self.heading.configure(background='#CDCDCD', foreground='#364156')
+        self.heading.pack(side=TOP, expand=True)
 
-if __name__ == "__main__":
-   
-    # dictionary to label all the CIFAR-10 dataset classes.
+        pickle_in = open("traffic_model.p", "rb")  # rb = READ BYTE
+        self.model = pickle.load(pickle_in)
+        # model.summary()
 
-    # initialise GUI
-    top = tk.Tk()
-    top.geometry('800x600')
-    top.title('Image Classification CIFAR10')
-    top.configure(background='#CDCDCD')
-    label = Label(top, background='#CDCDCD', font=('arial', 15, 'bold'))
-    sign_image = Label(top)
+    def get_classname(self, class_no):
+        data = pd.read_csv('labels.csv')
+        classname = None
+        for class_id, class_name in data.values:
+            if class_no == class_id:
+                classname = class_name
 
-    upload = Button(top, text="Upload an image", command=upload_image,
-                    padx=10, pady=5)
-    upload.configure(background='#364156', foreground='white',
-                     font=('arial', 10, 'bold'))
-    upload.pack(side=BOTTOM, pady=50)
-    sign_image.pack(side=BOTTOM, expand=True)
-    label.pack(side=BOTTOM, expand=True)
-    heading = Label(top, text="Image Classification CIFAR10",
-                    pady=20, font=('arial', 20, 'bold'))
-    heading.configure(background='#CDCDCD', foreground='#364156')
-    heading.pack()
-    top.mainloop()
+        return classname
+
+    def classify(self, file_path):
+        image = Image.open(file_path)
+        image = image.resize((32, 32))
+        image = image.convert('1')
+        image = np.expand_dims(image, axis=0)
+        image = np.array(image)
+        image = image.reshape((1, 32, 32, 1))
+        pred = self.model.predict([image])[0]
+        accuracy = np.amax(pred) * 100
+        accuracy = 'Accuracy:' + str(accuracy) + '%'
+        self.accuracy_label.configure(foreground='#011638', text=accuracy)
+        pred = np.argmax(pred)
+        class_name = self.get_classname(pred)
+        self.class_name.configure(foreground='#011638', text=class_name)
+        text = 'This is a ' + str(class_name)
+        self.definition.configure(foreground='#011638', text=text)
+
+    def show_classify_button(self, file_path):
+        classify_b = Button(self, text="Classify Image",
+                            command=lambda: self.classify(file_path), padx=10, pady=5)
+        classify_b.configure(background='#364156', foreground='white',
+                             font=('arial', 10, 'bold'))
+        classify_b.place(relx=0.79, rely=0.46)
+
+    def upload_image(self):
+        try:
+            file_path = filedialog.askopenfilename()
+            uploaded = Image.open(file_path)
+            uploaded.thumbnail(
+                ((self.winfo_width()/2.25), (self.winfo_height()/2.25)))
+            im = ImageTk.PhotoImage(uploaded)
+            self.class_image.configure(image=im)
+            self.class_image.image = im
+            self.class_name.configure(text='')
+            self.definition.configure(text='')
+            self.accuracy_label.configure(text='')
+            self.show_classify_button(file_path)
+        except:
+            pass
+
