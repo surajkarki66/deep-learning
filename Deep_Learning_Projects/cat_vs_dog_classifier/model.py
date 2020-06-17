ad4b09bf34f17a5b76dbdc2ec219fc89a2c434ec
Suraj Karki <suraj.karki500@gmail.com>
2020-06-17 08:48:32 +0545

    Handled exception

--- Deep_Learning_Projects/cat_vs_dog_classifier/model.py
+++ Deep_Learning_Projects/cat_vs_dog_classifier/model.py
@@ -4,47 +4,56 @@ from tensorflow.keras.layers import Conv2D, MaxPool2D, Dense, Flatten, Dropout
 from tensorflow.keras.applications.vgg19 import VGG19
 
 
-def model(input_shape=(128, 128, 3), classes = 2):
-    x_input = tf.keras.layers.Input(shape=input_shape)
-    x = tf.keras.layers.Conv2D(32, (3, 3), activation='relu')(x_input)
-    x = tf.keras.layers.BatchNormalization()(x)
-    x = tf.keras.layers.MaxPooling2D(pool_size=(2, 2))(x)
-    x = tf.keras.layers.Dropout(0.25)(x)
+def simple_model(input_shape=None, classes = None):
+	try:
+		x_input = tf.keras.layers.Input(shape=input_shape)
+		x = tf.keras.layers.Conv2D(32, (3, 3), activation='relu')(x_input)
+		x = tf.keras.layers.BatchNormalization()(x)
+		x = tf.keras.layers.MaxPooling2D(pool_size=(2, 2))(x)
+		x = tf.keras.layers.Dropout(0.25)(x)
 
-    x = tf.keras.layers.Conv2D(64, (3, 3), activation='relu')(x)
-    x = tf.keras.layers.BatchNormalization()(x)
-    x = tf.keras.layers.MaxPooling2D(pool_size=(2, 2))(x)
-    x = tf.keras.layers.Dropout(0.25)(x)
+		x = tf.keras.layers.Conv2D(64, (3, 3), activation='relu')(x)
+		x = tf.keras.layers.BatchNormalization()(x)
+		x = tf.keras.layers.MaxPooling2D(pool_size=(2, 2))(x)
+		x = tf.keras.layers.Dropout(0.25)(x)
 
-    x = tf.keras.layers.Conv2D(128,(3,3),activation='relu')(x)
-    x = tf.keras.layers.BatchNormalization()(x)
-    x = tf.keras.layers.MaxPooling2D(pool_size=(2, 2))(x)
-    x = tf.keras.layers.Dropout(0.25)(x)
+		x = tf.keras.layers.Conv2D(128,(3,3),activation='relu')(x)
+		x = tf.keras.layers.BatchNormalization()(x)
+		x = tf.keras.layers.MaxPooling2D(pool_size=(2, 2))(x)
+		x = tf.keras.layers.Dropout(0.25)(x)
 
-    x = tf.keras.layers.Flatten()(x)
-    x = tf.keras.layers.Dense(512, activation='relu')(x)
-    x = tf.keras.layers.BatchNormalization()(x)
-    x = tf.keras.layers.Dropout(0.25)(x)
-    x = tf.keras.layers.Dense(classes, activation='softmax')(x)
-    model  = tf.keras.models.Model(inputs = x_input, outputs = x)
-    
-    return model
+		x = tf.keras.layers.Flatten()(x)
+		x = tf.keras.layers.Dense(512, activation='relu')(x)
+		x = tf.keras.layers.BatchNormalization()(x)
+		x = tf.keras.layers.Dropout(0.25)(x)
+		x = tf.keras.layers.Dense(classes, activation='softmax')(x)
+		model  = tf.keras.models.Model(inputs = x_input, outputs = x)
+
+		return model
+
+	except ValueError:
+		print('Please provide to Input either a `shape` or a `tensor` argument. Note that `shape` does not include the batch dimension.')
 
 
-# VGG19 
-def define_model():
-    model =  VGG19(include_top=False, input_shape=(224, 224, 3))
-    # mark loaded layers as non trainable
-    for layer in model.layers:
-        layer.trainable = False
 
-    # new classifier layer
-    flat = Flatten()(model.layers[-1].output)
-    dense = Dense(128, activation='relu', kernel_initializer='he_uniform')(flat)
-    output = Dense(1, activation='sigmoid')(dense)
+# VGG19
+def vgg19_model(input_shape=None, classes=None):
+	try:
+		model =  VGG19(include_top=False, input_shape=input_shape)
+		# mark loaded layers as non trainable
+		for layer in model.layers:
+			layer.trainable = False
 
-    #define the model
-    model = Model(inputs=model.inputs, outputs=output)
+		# new classifier layer
+		flat = Flatten()(model.layers[-1].output)
+		dense = Dense(128, activation='relu', kernel_initializer='he_uniform')(flat)
+		output = Dense(classes, activation='softmax')(dense)
 
-    return model
+		#define the model
+		model = Model(inputs=model.inputs, outputs=output)
+
+		return model
+
+	except ValueError:
+			print('Please provide to Input either a `shape` or a `tensor` argument. Note that `shape` does not include the batch dimension.')
 
